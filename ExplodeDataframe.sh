b626473240d46ef747b97f2ab321ede4d6473db7
vartikabisht6197 <vartika@annogen.bio>
2023-03-27 13:58:34 +0200

    Normalisation and Exploding scripts added. The original normalisation.py is now split into two parts.

---
+++ ExplodeDataframe.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+# AUTHOR / DATE
+# Vartika Bisht; March 22, 2023
+
+## ExplodeDataframe.sh : Bash scipt for exploding dataframe
+## This script first determine the column numbers using the column names specified for exploding.
+## It then splits the dataframe into rows which have to be exploded and which need no exploding.
+## Then, it iteratively goes through all the columns which have to be exploded and do it for each rows.
+## Lastly, it joins the exploded dataframe with the data frame which need no exploding and finaly removes duplicates.
+
+## Input File Format:
+## Tab seperated file
+## Column names as the first row of the file
+## Colnames specified in the -c flag of TotalCounts.sh must exist is all files specified under -f
+
+## Output File Format:
+## Tab seperated file
+## Column names as the first row of the file
+
+
+## bash ExplodeDataframe.sh -f '/HDD/data2/scratch/vartika/projects/VB221020_HLHS_GDG11_X57/SuRE-IndelPipelineOutput-X57-221207/filtered_counts_table/15_final_counts_EMP/chr22.txt.gz' -c 'SNP_ABS_POS SNP_SEQ SNP_PARENT SNP_VAR SNP_TYPE SNP_SUBTYPE SNP_ABS_POS_hg19 SNP_ID' -o outfile.txt.gz
+
+
+OPTIND=1         
+
+SCRIPTNAME="ExplodeDataframe.sh"
+
+# Usage 
+USAGE=
+usage() {
+  echo >&2 "usage: ${SCRIPTNAME} -?:h:f:c:o:"
+  echo >&2 "OPTIONS:"
+  echo >&2 "  -f: SuRE Count file of the format .gz [required]"
+  echo >&2 "  -c: Names of the columns with comma seperated files which have to be exploded [required]"
+  echo >&2 "  -o: output file, in gz format [required]"
+  echo >&2 "  -h: print this message"
+  echo >&2 ""
+  exit 1;
+}
+# Opts
+while getopts "?:h:C:f:c:o:" opt; do
+  case $opt in
+    f)
+      file=$OPTARG;
+      ;;
+    c)
+      commacols=$OPTARG;
+      ;;
+    o)
+      outfile=$OPTARG;
+      ;;
+    h)
+      usage;
+      ;;
+    \?)
+      echo "option not recognized: "$opt
+      usage
+      ;;
+  esac
+done
+shift $(( OPTIND - 1 ))
+
+#Check if output dir exists, if not, then create it
+outdir="$(dirname "$outfile")"
+if [ -d $outdir ] 
+then
+    echo "$outdir directory exists." 
+else
+    mkdir $outdir
+    echo "$outdir directory does not exists, now it is created."
+fi
+
+
+function join_by {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
+
+# Determine the column numbers usinh the column names
+# Store it the commacolsnums variable
+commacolsnums=()
+for col in $commacols
+do
+    colnum=($(head -1  <(zcat $file) | sed 's/\t/\n/g' | nl | grep $col | awk '{print $1}'))
+    commacolsnums+=($colnum)
+done
+
+echo "Preparing dataframe for explosion ..."
+
+# Split the dataframe into two
+# Dataframe with rows to be exploded
+cat <(zcat $file | head -n 1)  <(gawk -vcommacheck=${commacolsnums[0]} '{if($commacheck ~ /,/ ){print $0}}' <(zcat $file | tail -n +2)) > $TMP/commasubset.txt
+# Dataframe with no rows to be exploded
+gawk -vcommacheck=${commacolsnums[0]} '{if($commacheck !~ /,/ ){print $0}}' <(zcat $file | tail -n +2) > $TMP/non_commasubset.txt
+
+
+
+# The columns without comma are concatenated with the delimiter "|" and together form the first column
+# 2,3,4...NF columns have comma seperated values.
+# Say, you have 3 columns wuth 4 values, such that last column has comma seperated values, this step would: V1 V2 V3,V4 --> V1|V2 V3,V4
+paste -d$'\t' <(cut --complement -d$'\t' -f$(join_by , "${commacolsnums[@]}" ) $TMP/commasubset.txt | awk -vFS="\t" -vOFS="|" '$1=$1' -) \
+     <(cut -d$'\t' -f$(join_by , "${commacolsnums[@]}" ) $TMP/commasubset.txt) > $TMP/ready_to_explode_commasubset.txt
+
+echo "Ready to explode!"
+
+# We exploded columns 2,3,4...NF.
+# First split all columns with commas, then write them down in seperate rows with the 1st column same.
+# The Output field sep has been changed to "|" , so that all rows are now seperated by delimiter |
+# We can then change the delimited from "|" to "\t"
+awk -vFS="\t" -vOFS="|" '{
+    n=0;
+    for(i=2;i<=NF;i++) {
+        t=split($i,a,",");if(t>n) n=t};
+    for(j=1;j<=n;j++) {
+        printf "%s",$1;
+        for(i=2;i<=NF;i++) {
+            split($i,a,",");printf "|%s",(a[j]?a[j]:a[1])
+            };
+        print ""
+        }
+    }' $TMP/ready_to_explode_commasubset.txt > $TMP/exploded_commasubset.txt
+
+
+
+# Concatenate the exploded subset with the non comma subset
+# Make sure that the relative column numbers are same
+paste -d$'\t' <(cut --complement -d$'\t' -f$(join_by , "${commacolsnums[@]}" ) $TMP/non_commasubset.txt ) \
+     <(cut -d$'\t' -f$(join_by , "${commacolsnums[@]}" ) $TMP/non_commasubset.txt) > $TMP/ordered_non_commasubset.txt
+
+cat <(awk -vOFS="\t" -vFS="|" '$1=$1' $TMP/exploded_commasubset.txt) $TMP/ordered_non_commasubset.txt | awk -vFS="\t" -vOFS="\t" '!seen[$0]++' - | gzip -c - > $outfile
+
+echo "Your dataframe is exploded, duplicated rows have been removed and the final file is written to" $outfile
---
+++ Normalisation.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+# AUTHOR / DATE
+# Vartika Bisht; March 22, 2023
+
+
+
+## bash Normalisation.sh -f 'outfile.txt.gz' -i 'count' -c 'X57_AC16_220726_A X57_AC16_220726_B' -s 'SNP_ABS_POS_hg19' -o norm.out.txt.gz -t SuRE_X57_TC.txt
+
+
+OPTIND=1         
+
+SCRIPTNAME="Normalisation.sh"
+
+#Default
+prefix=ipcr.norm.sum
+
+# Usage 
+USAGE=
+usage() {
+  echo >&2 "usage: ${SCRIPTNAME} -?:h:f:c:i:s:p:t:o:"
+  echo >&2 "OPTIONS:"
+  echo >&2 "  -f: SuRE Count file of the format .gz [required]"
+  echo >&2 "  -c: Names of the cDNA columns [required]"
+  echo >&2 "  -i: Names of the iPCR column [required]"
+  echo >&2 "  -s: Names of the SNP position column [required]"
+  echo >&2 "  -p: prefix for normalised cDNA columns"
+  echo >&2 "  -t: Total count file, txt file [required]"
+  echo >&2 "  -o: output file, in gz format [required]"
+  echo >&2 "  -h: print this message"
+  echo >&2 ""
+  exit 1;
+}
+# Opts
+while getopts "?:h:f:c:i:s:t:o:" opt; do
+  case $opt in
+    f)
+      file=$OPTARG;
+      ;;
+    c)
+      cDNA=$OPTARG;
+      ;;
+    i)
+      iPCR=$OPTARG;
+      ;;
+    s)
+      snppos=$OPTARG;
+      ;;
+    t)
+      totalcount=$OPTARG;
+      ;;
+    o)
+      outfile=$OPTARG;
+      ;;
+    h)
+      usage;
+      ;;
+    \?)
+      echo "option not recognized: "$opt
+      usage
+      ;;
+  esac
+done
+shift $(( OPTIND - 1 ))
+
+function join_by {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
+
+## Remove all rows without mutations
+echo "Remove all fragments without any mutations.."
+snpposcolnum=($(head -1  <(zcat $file) | sed 's/\t/\n/g' | nl | grep $snppos | awk '{print $1}'))
+awk -vFS="\t" -vOFS="\t" -vsnpposcolnum=$snpposcolnum '$snpposcolnum != ""' <(zcat $file) | gzip -c - > $TMP/rm_noMut.txt.gz
+file=$TMP/rm_noMut.txt.gz
+echo "Ready to process!"
+
+echo "To scale the data for processing, we divide iPCR and cDNA counts by their respective depths, i.e. Total_iPCR and Total_cDNA-1,Total_cDNA-2,Total_cDNA-3 ... "
+echo "To normalise the enrichment for each fragment, we divide scaled cDNA by scaled iPCR for each fragment.."
+echo "Hence, for each fragments i, the scaled and normalised enrichment wrt cDNA sample k, can be written as (cDNA-k-i/iPCR-i) * (Total_cDNA-k/Total_iPCR) "
+
+## Define the multiplication factor
+echo "Total_cDNA-k/Total_iPCR can be interpreated as a multiplication factor for cDNA sample k"
+echo "Calculating multiplication factor for all cDNA samples.."
+ipcrcolnum=($(head -1   $totalcount | sed 's/\t/\n/g' | nl | grep $iPCR | awk '{print $1}'))
+ipcr_total=($(awk -vFS="\t" -vipcrcolnum=$ipcrcolnum 'NR>1{print $ipcrcolnum}' $totalcount))
+## Make sure that the relative order of cDNA names in $totalcount is the same as it is in $file.
+## Determine the cDNA column number in the file
+cDNAcolnum=()
+for col in $cDNA
+do
+  colnum=($(head -1 <(zcat $file) | sed 's/\t/\n/g' | nl | grep $col | awk '{print $1}'))
+  cDNAcolnum+=($colnum)
+done
+## Rearrage $totalcount
+cut -d$'\t' -f${ipcrcolnum} $totalcount > $TMP/Rearrage_totalcount.txt
+for col in $(head -n1 <(cut -d$'\t' -f$(join_by , "${cDNAcolnum[@]}") <(zcat $file)))
+do
+    colnum=($(head -1 $totalcount | sed 's/\t/\n/g' | nl | grep $col | awk '{print $1}'))
+    paste -d$'\t' <(cat $TMP/Rearrage_totalcount.txt) <(cut -d$'\t' -f$colnum $totalcount) > tmp
+    mv tmp $TMP/Rearrage_totalcount.txt
+done
+# write multiplication factor as total iPCR / total cDNA in a file for each cDNA 
+awk -vFS="\t" -vOFS="\t" -vipcr_total=$ipcr_total 'NR==1 {$0} ;NR>1{for(i=1;i<=NF;++i){$i = ipcr_total/$i }}1' <(cut -d$'\t' --complement -f1 $TMP/Rearrage_totalcount.txt) > $TMP/MF.txt
+echo "Multiplication factor for all cDNA samples calculated."
+
+## Normalise cDNA and add new column 
+echo "Starting normalisation.."
+echo "First, dividing cDNA count by iPCR count for each fragment wrt each cDNA sample (cDNA-k-i/iPCR-i) .."
+## Determine the iPCR column number in the file
+ipcrcolnum=($(head -1   <(zcat $file) | sed 's/\t/\n/g' | nl | grep $iPCR | awk '{print $1}'))
+
+## Subset the file to only have iPCR and cDNA columns
+## The first column will be the iPCR column and all the other would be cDNA
+## The relative order amongst the cDNA samples is the same as in the original file.
+paste -d$'\t' <(cut -d$'\t' -f$ipcrcolnum <(zcat $file)) <(cut -d$'\t' -f$(join_by , "${cDNAcolnum[@]}") <(zcat $file)) > $TMP/subset_file.txt
+
+## Divide cDNA by iPCR for all rows.
+awk -vFS="\t" -vOFS="\t" -vprefix=$prefix 'NR==1 {for(i=2;i<=NF;++i){$i = prefix"."$i}} ;NR>1{for(i=2;i<=NF;++i){$i = $i/$1}}1' $TMP/subset_file.txt | cut -d$'\t' -f2- - > $TMP/${prefix}.file.txt
+
+
+echo "Now, multiplying the multiplication factor to each cDNA sample as calculated previously, i.e. (cDNA-k-i/iPCR-i)  * (Total_cDNA-k/Total_iPCR) "
+## The relative order of columns in the MF.txt and ${prefix}.file.txt is same.
+## $TMP/${prefix}.file.txt has the same relative order as original file.
+## We have already rearrange $TMP/MF.txt to have same order as original file.
+## Multiple the respective multiplication factors and add these columns to the file
+paste -d$'\t' <(zcat $file) <(cat <(head -n 1 $TMP/${prefix}.file.txt) <(awk -vFS="\t" -vOFS="\t" 'NR==1{cols = split($0,m);next}; NR>1{for(i=1; i<=NF; i++){$i = sprintf("%.5f", $i*m[i])}}1' <(tail -n +2 $TMP/MF.txt) <(tail -n +2 $TMP/${prefix}.file.txt))) | gzip -c - > $outfile
+
+
+echo "Normalisation done."
+echo "Normalised file saved to" $outfile
